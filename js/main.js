835a10742dc0def766b9fca7880e5bf9291bb831
Denis Szczerbowski <den_eezzz@mail.ru>
2021-03-26 02:24:55 +1100

    create: map and media rules for hotel section

--- js/main.js
+++ js/main.js
@@ -12,3 +12,28 @@ const swiper = new Swiper('.swiper-container', {
     onlyInViewport: false,
   },
 });
+ymaps.ready(init);
+        function init(){
+            var myMap = new ymaps.Map("map", {
+                center: [7.890703, 98.294772],
+                zoom: 16
+            });
+        
+myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
+            hintContent: 'DoubleTree by Hilton Phuket Banthai Resort',
+            balloonContent: 'DoubleTree by Hilton Phuket Banthai Resort'
+        }, {
+            // Опции.
+            // Необходимо указать данный тип макета.
+            iconLayout: 'default#image',
+            // Своё изображение иконки метки.
+            iconImageHref: 'img/pin.svg',
+            // Размеры метки.
+            iconImageSize: [17, 30],
+            // Смещение левого верхнего угла иконки относительно
+            // её "ножки" (точки привязки).
+            iconImageOffset: [-8, -30]
+        });
+        myMap.geoObjects
+        .add(myPlacemark)
+        }
