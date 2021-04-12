fe073db2fe630c333e3ee659da2b37478cb80fdc
Denis Szczerbowski <den_eezzz@mail.ru>
2021-04-13 02:40:46 +1100

    create: added modal booking window

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-const hotelSlider = new Swiper('.hotel-slider', {
+$(document).ready(function () {
+  const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
   // Navigation arrows
@@ -34,10 +35,32 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   },
 });
 
-var menuButton = document.querySelector('.menu-button');
-menuButton.addEventListener('click', function () {
-console.log('клик по кнопке');
-document.querySelector('.navbar-bottom').classList.toggle('navbar-bottom--visible') ;  
+var menuButton = $('.menu-button');
+menuButton.on('click', function () {
+$('.navbar-bottom').toggleClass('navbar-bottom--visible') ;  
+});
+var modalButton = $('[data-toggle=modal');
+var closeModalButton = $('.modal__close');
+modalButton.on('click', openModal);
+closeModalButton.on('click', closeModal);
+
+function openModal() {
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.addClass('modal__overlay--visible');
+  modalDialog.addClass('modal__dialog--visible');
+}
+
+function closeModal(event) {
+  event.preventDefault();
+  var modalOverlay = $('.modal__overlay');
+  var modalDialog = $('.modal__dialog');
+  modalOverlay.removeClass('modal__overlay--visible');
+  modalDialog.removeClass('modal__dialog--visible');
+  }
+  
+$(document).keyup(function(e) {
+if (e.keyCode === 27) $('.modal__close').click();   // esc
 });
 
-$('.newsletter').parallax({imageSrc: 'img/newsletter-bg-2.jpg'});
+});
