51730f4399e1c8b71886b9df3b3faab94c59b297
Denis Szczerbowski <den_eezzz@mail.ru>
2021-04-15 04:39:48 +1100

    create: forms validations

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ var menuButton = $('.menu-button');
 menuButton.on('click', function () {
 $('.navbar-bottom').toggleClass('navbar-bottom--visible') ;  
 });
-var modalButton = $('[data-toggle=modal');
+var modalButton = $('[data-toggle=modal]');
 var closeModalButton = $('.modal__close');
 modalButton.on('click', openModal);
 closeModalButton.on('click', closeModal);
@@ -58,9 +58,45 @@ function closeModal(event) {
   modalOverlay.removeClass('modal__overlay--visible');
   modalDialog.removeClass('modal__dialog--visible');
   }
-  
+// esc
 $(document).keyup(function(e) {
-if (e.keyCode === 27) $('.modal__close').click();   // esc
+if (e.keyCode === 27) $('.modal__close').click();   
 });
+
+// Forms processing 
+$(".form").each(function() {
+  $(this).validate({
+  errorClass: "invalid",
+  messages: { 
+    name: {
+      required: "Please specify your name",
+      minlength: "Please enter at least 2 simbols",
+    },
+    phone: {
+      required: "Phone number required",
+      minlength: "Please enter at least 11 digits",
+    },
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+  }
+  });
+  
+  });
+  
+// Forms processing 
+$(".subscribe").validate({
+  errorClass: "invalid",
+  messages: { 
+    email: {
+      required: "We need your email address to contact you",
+      email: "Your email address must be in the format of name@domain.com"
+    },
+  }
+  });
+  
+// Phone mask
+$('[data-phone=digit]').mask('+7 (999) 999-99-99');
 
 });
